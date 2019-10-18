751b79b9f41b180543896f1e89f49137564a3899
Oana Moisa <oana_moisa29@yahoo.com>
2019-10-18 13:42:16 +0300

    third commit

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Let's chat\n");
+    char userInput[100];
+    char name[20];
+    int age;
+    gets(userInput);
+    printf("%s to you too.  I am HAL, what is your first name?\n", userInput);
+    scanf("%s", name);
+    printf("Oh hi, %s. How old are you?\n",name);
+    scanf("%d", &age);
+    printf("Wow. %d is quite something\n",age);
+    gets(userInput);
+    gets(userInput);
+    printf("sorry, not sure what this \"%s\" means",userInput);
     return 0;
 }
