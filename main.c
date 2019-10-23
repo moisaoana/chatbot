0fbc2244b2e3f8b464f46b3c40e5f0615e6500de
Oana Moisa <oana_moisa29@yahoo.com>
2019-10-23 23:11:44 +0300

    homework

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 int main() {
     printf("Let's chat\n");
     char userInput[100];
-    char name[20];
-    int age;
+    char name[20], country[20];
+    int age,years;
     gets(userInput);
     printf("%s to you too.  I am HAL, what is your first name?\n", userInput);
     scanf("%s", name);
@@ -13,6 +13,18 @@ int main() {
     printf("Wow. %d is quite something\n",age);
     gets(userInput);
     gets(userInput);
-    printf("sorry, not sure what this \"%s\" means",userInput);
+    printf("What country are you from?\n");
+    scanf("%s", country);
+    printf("Oh, I've heard of %s. Such a lovely country!\n");
+    gets(userInput);
+    gets(userInput);
+    printf("Can you recommend some attractions?\n");
+    gets(userInput);
+    printf("Thank you! That sounds amazing!\n");
+    printf("What are you studying at university?\n");
+    gets(userInput);
+    printf("That sounds difficult! How many years does it take to major in %s?\n ", userInput);
+    scanf("%d", &years);
+    printf("Well, good luck!");
     return 0;
 }
